56ea4202422383a6f52fac1574cebd28cd609331
Spectrum-Man <112392292+MyFa-yf@users.noreply.github.com>
2025-06-04 06:52:34 +0700

    Add files via upload

---
+++ App.py
@@ -0,0 +1,15 @@
+print(2 ** 4)
+print(2 * 3)
+print(2 / 3)
+print(3 // 2)
+print(2 % 3)
+x = 20
+x -= 6
+print(x)
+M = int(3 * 3 - 3 / 3 + 3 ** 3)
+print(M)
+
+G = 3000000 / 5000
+print(G)
+k = 50 + 25 + 50 + 30 + 30 + 75 + 15
+print(k)
